897a0a6f6b0918eb950279611a884743fa7c4b4f
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-04 18:58:06 +0000

    adds Role model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_185412) do
+ActiveRecord::Schema.define(version: 2022_02_04_185658) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2022_02_04_185412) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+    t.string "character_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
